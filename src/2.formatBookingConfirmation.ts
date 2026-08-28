dae276304c52d753da8955d1610c8d60391c2a9a
ahassh1 <ahasanhabibpolash420@gmail.com>
2026-08-28 23:33:17 +0600

    formatBookingConfirmation little error fixed

--- src/2.formatBookingConfirmation.ts
+++ src/2.formatBookingConfirmation.ts
@@ -4,11 +4,7 @@ interface Booking{
     time: string
 }
 function formatBookingConfirmation(booking:Booking):string {
-  return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time} PM.`
+  return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time}.`
 }
  const bookingUserDetails = { name: "Aisha", guests: 4, time: "7:00 PM" }
  console.log(formatBookingConfirmation(bookingUserDetails));
- 
-
-// { name: "Aisha", guests: 4, time: "7:00 PM" }
-// "Aisha's table for 4 guests is confirmed at 7:00 PM."
